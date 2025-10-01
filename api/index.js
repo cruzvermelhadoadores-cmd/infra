46c21d0c61dea4e5d9f08222494582d5828557c7
cruzvermelhadoadores-cmd <cruzvermelhadoadores@gmail.com>
2025-10-01 07:59:54 +0000

    atuall_0

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 
-require('dotenv').config();
+require('dotenv').config({ override: true });
 
 const app = express();
 app.use(express.json());
